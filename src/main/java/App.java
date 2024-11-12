053ecf7930bfcaf3593daa66b1cec57ef60451be
Roman Orlov <main.r.orlov@gmail.com>
2024-11-12 15:59:57 +0300

    Release Cat bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorld1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(helloWorld1.getMessage());
+        HelloWorld helloWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        
+        System.out.println(helloWorld1 == helloWorld2);
+        System.out.println(cat1 == cat2);
+        
     }
 }
